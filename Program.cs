a538014062f5e60cd1c6f2057e8f08c92f414768
elorasmith <eloraasmith@gmail.com>
2025-02-25 21:09:32 -0700

    String and Array

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 
 // Elora Smith, 2/25/25, Lab 5 Pig Latin/ Encoder
 
-Console.WriteLine("Welcome to Pig Latin Cryptogram! Give me a phrase.");
+Console.WriteLine("Welcome to Pig Latin Cryptogram! Give me a sentence or phrase.");
 string? phrase = Console.ReadLine();
+string[] words = phrase.Split(' ');
+
+foreach (string word in words)
+    Console.WriteLine(word);
+
+
 Console.WriteLine("Let's manipulate your phrase:");
--- obj/Debug/net8.0/lab-5-piglatin-encoder-elorasmith.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-5-piglatin-encoder-elorasmith.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-5-piglatin-encoder-elorasmith")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc63b53150b0a27526f3861225ceadb4f21697d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6585db76b44c63b703b7c35402796a0b83cb13")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-5-piglatin-encoder-elorasmith")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-5-piglatin-encoder-elorasmith")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
