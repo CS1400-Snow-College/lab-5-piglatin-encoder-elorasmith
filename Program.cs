c6e80115123053dce2308d7ea78de9e8d4a482bf
elorasmith <eloraasmith@gmail.com>
2025-02-25 22:08:58 -0700

    Pig Latin attempt 1

--- Program.cs
+++ Program.cs
@@ -3,11 +3,22 @@
 // Elora Smith, 2/25/25, Lab 5 Pig Latin/ Encoder
 
 Console.WriteLine("Welcome to Pig Latin Cryptogram! Give me a sentence or phrase.");
+
 string? phrase = Console.ReadLine();
 string[] words = phrase.Split(' ');
 
 foreach (string word in words)
+{
+    if (word[0]=='a' || word[0]=='e' || word[0]=='i' || word[0]=='o' || word[0]=='u')
+        word = word + "way";
+
+}
+    
+
+
+
+foreach (string word in words)
     Console.WriteLine(word);
 
-
-Console.WriteLine("Let's manipulate your phrase:");
+enum Consonant {b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z}
+enum Vowel {a,e,i,o,u}
